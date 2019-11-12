a223ffc2ea6158838b426022c62911a85e7f33cb
aurelie mathys <mathyau@gmail.com>
2019-11-12 17:17:51 -0500

    challenge done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def delete
+  def destroy
     @task = Task.find(params[:id])
     @task.destroy
     redirect_to index_path
